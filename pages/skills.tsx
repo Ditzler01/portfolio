acde9c3beac1bdf8b36231b5c0e04a18f31aa068
Ditzler01 <71181685+Ditzler01@users.noreply.github.com>
2023-01-13 01:53:23 +0800

    Skill UI and text (Not Done Yet)

---
+++ pages/skills.tsx
@@ -0,0 +1,105 @@
+export default function Skills()
+{
+    return (
+        <>
+            <div className="w-full py-12">
+                <div className="flex lg:flex-row flex-col px-4">
+                    <div className="lg:w-[50%] w-full flex items-center">
+                        <img src="/images/database-final.png" className="w-full h-max"/>
+                    </div>
+                    <div className="2xl:w-[45%] lg:w-[50%] w-full lg:mt-0 flex justify-center flex-col lg:mt-0 mt-12 lg:px-0 px-8">
+                        <p className="2xl:text-7xl lg:text-6xl md:text-6xl text-5xl text-secondary font-light lg:text-left text-center">Database</p>
+                        <div className="w-full mt-12">
+                            <p className="text-xl lg:text-left text-justify">
+                                I have been using MySQL for a very long time. I have learned MySQL during my term project
+                                 in one of my subjects in school way back during my 2nd year college. MySQL is one of my go to
+                                 database when creating a web application.
+                            </p>
+                            <p className="text-xl lg:text-left text-justify mt-4">
+                                Firebase is one of the recent databases I have used during the time when I was learning
+                                 mobile application development. It is very different from MySQL as it has a JSON format style
+                                 and it is a realtime database
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex lg:flex-row flex-col px-4 mt-7 justify-end">
+                    <div className="2xl:w-[45%] lg:w-[50%] lg:pl-22 2xl:pl-0 w-full lg:mt-0 flex justify-center flex-col lg:mt-0 mt-12 lg:px-0 px-8 lg:order-1 order-2">
+                        <p className="2xl:text-7xl lg:text-6xl md:text-6xl text-5xl text-secondary font-light lg:text-left text-center">Mobile Development</p>
+                        <div className="w-full mt-12">
+                            <p className="text-xl lg:text-left text-justify">
+                                Flutter and Andriod Studio are the one I use to develop a mobile application. I started developing mobile application during my third
+                                 year college. I first used Android Studio but soon switched to Flutter when I created my own project. The mobile application project
+                                 I'm creating is still ongoing and I still continue to learn mobile development up to this day.   
+                            </p>
+                        </div>
+                    </div>
+                    <div className="lg:w-[50%] w-full flex items-center lg:order-2 order-1">
+                        <img src="/images/mobile-dev.png" className="w-full h-max"/>
+                    </div>
+                </div>
+                <div className="flex lg:flex-row flex-col px-4 mt-7">
+                    <div className="lg:w-[50%] w-full flex items-center">
+                        <img src="/images/API.svg" className="w-full h-max"/>
+                    </div>
+                    <div className="2xl:w-[45%] lg:w-[50%] w-full lg:mt-0 flex justify-center flex-col lg:mt-0 mt-12 lg:px-0 px-8">
+                        <p className="2xl:text-7xl lg:text-6xl md:text-6xl text-5xl text-secondary font-light lg:text-left text-center">API</p>
+                        <div className="w-full mt-12">
+                            <p className="text-xl lg:text-left text-justify">
+                                I have experienced creating an API using Java and MySQL during my second year college. I have learned the use of
+                                 POST, GET and SET which are the basic HTTP methods. Not only creating an API but also integrating an API from
+                                 different web services such as Google Maps, Map Box and Steam to name a few.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex lg:flex-row flex-col px-4 mt-7 justify-end">
+                    <div className="2xl:w-[45%] lg:w-[50%] lg:pl-22 2xl:pl-0 w-full lg:mt-0 flex justify-center flex-col lg:mt-0 mt-12 lg:px-0 px-8 lg:order-1 order-2">
+                        <p className="2xl:text-7xl lg:text-6xl md:text-6xl text-5xl text-secondary font-light lg:text-left text-center">Game Development</p>
+                        <div className="w-full mt-12">
+                            <p className="text-xl lg:text-left text-justify">
+                                I started learning Game Development as one of my hobby. I learned Unity Game Engine during the early days of
+                                 COVID-19 and was able to finish some games. Game Development is difficult to learn aside from coding as 
+                                 it is one of my specialty. Music/SFX, Arts, Ideas and Creativity are the skills that was improved
+                                 while learning Game Development.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="lg:w-[50%] w-full flex items-center lg:order-2 order-1">
+                        <img src="/images/game-dev.png" className="w-full h-max"/>
+                    </div>
+                </div>
+                <div className="flex lg:flex-row flex-col px-4 mt-7">
+                    <div className="lg:w-[50%] w-full flex items-center pr-4">
+                        <img src="/images/art.svg" className="w-full h-max"/>
+                    </div>
+                    <div className="2xl:w-[45%] lg:w-[50%] w-full lg:mt-0 flex justify-center flex-col lg:mt-0 mt-12 lg:px-0 px-8">
+                        <p className="2xl:text-7xl lg:text-6xl md:text-6xl text-5xl text-secondary font-light lg:text-left text-center">Multimedia Technologies</p>
+                        <div className="w-full mt-12">
+                            <p className="text-xl lg:text-left text-justify">
+                                Photo editing and video editing are my skills when it comes to digital arts. In our church, I'm the one whose responsible
+                                 for creating themes and invitation images for our events. Photoshop and Sonny Vegas are the applications I use for photo
+                                 and video editing.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex lg:flex-row flex-col px-4 mt-7 justify-end">
+                    <div className="2xl:w-[45%] lg:w-[50%] lg:pl-22 2xl:pl-0 w-full lg:mt-0 flex justify-center flex-col lg:mt-0 mt-12 lg:px-0 px-8 lg:order-1 order-2">
+                        <p className="2xl:text-7xl lg:text-6xl md:text-6xl text-5xl text-secondary font-light lg:text-left text-center">Web Automation</p>
+                        <div className="w-full mt-12">
+                            <p className="text-xl lg:text-left text-justify">
+                                Selenium (C#) is the one I use to automate websites. Web Automation has a large similarity when it comes to
+                                 web scraping. As soon as I learned web automation, learning web scraping is not so difficult for me. I explored
+                                 this field when I was creating a trading bot in Steam.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="lg:w-[50%] w-full flex items-center lg:order-2 order-1 px-8">
+                        <img src="/images/web-auto.svg" className="w-full h-max"/>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
