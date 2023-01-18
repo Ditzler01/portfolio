c6f7d96f44226a87d888fcdfd19a8ff20dc215bc
Ditzler01 <71181685+Ditzler01@users.noreply.github.com>
2023-01-18 13:39:26 +0800

    Projects UI (Unfinished)

--- components/layout.tsx
+++ components/layout.tsx
@@ -17,7 +17,8 @@ export default function Layout({ children }: Props)
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <title>Portfolio | Ditzler Santos</title>
             </Head>
-            <div className="font">
+
+            <div className="font w-full h-full flex flex-col relative">
                 <Navbar/>
                 { children }
             </div>
---
+++ pages/projects.tsx
@@ -0,0 +1,91 @@
+export default function Projects()
+{
+    return (
+        <>
+            <div className="w-full flex-1 flex lg:flex-row flex-col">
+                <div className="lg:w-[50%] w-full h-full flex justify-center items-center">
+                    <img src="/images/projects.svg" className="w-[70%]"/>
+                </div>
+                <div className="lg:w-[50%] w-full h-full flex flex-col justify-center py-20 my-5">
+                    <p className="2xl:text-7xl lg:text-6xl md:text-6xl text-5xl text-secondary font-light lg:text-left text-center lg:mt-0 mt-24">Projects</p>
+                    <div className="w-full mt-20 lg:px-0 px-20">
+                        <div className="flex flex-row items-center">
+                            <div className="py-2 px-2 shadow-[rgba(0,0,0,0.1)_0px_6px_24px_0px,_rgba(0,0,0,0.4)_0px_0px_0px_1px] rounded-full w-28">
+                                <p className="tex-xl font-[600] text-center">Simpllify</p>
+                            </div>
+                            <p className="font-[600] ml-5 text-[rgb(110,110,110)]">01/2023 - present</p>
+                        </div>
+                        
+                        <div className="lg:w-[85%] w-full mt-5">
+                            <p className="text-xl">
+                                Simplify is a website that has similarity to LinkedIn. It is one of my most recent freelance projects and our clients are from US. I'm
+                                responsible for backend development for this project and Next JS is the framework we are using to develop this website. 
+                            </p>
+                        </div>
+                    </div>
+                    <div className="w-full mt-10 lg:px-0 px-20">
+                        <div className="flex flex-row items-center">
+                            <div className="py-2 px-2 shadow-[rgba(0,0,0,0.1)_0px_6px_24px_0px,_rgba(0,0,0,0.4)_0px_0px_0px_1px] rounded-full w-28">
+                                <p className="tex-xl font-[600] text-center">MZBC Bot</p>
+                            </div>
+                            <p className="font-[600] ml-5 text-[rgb(110,110,110)]">09/2022 - present</p>
+                        </div>
+                        
+                        <div className="lg:w-[85%] w-full mt-5">
+                            <p className="text-xl">
+                                MZBC Bot is a mobile application to be used in our church. In this project, I'm currently using Flutter for development and Firebase for my backend.
+                                This project is currently on hold and will continue the development later this year.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="w-full mt-10 lg:px-0 px-20">
+                        <div className="flex flex-row items-center">
+                            <div className="py-2 px-2 shadow-[rgba(0,0,0,0.1)_0px_6px_24px_0px,_rgba(0,0,0,0.4)_0px_0px_0px_1px] rounded-full w-36">
+                                <p className="tex-xl font-[600] text-center">Local Fishers</p>
+                            </div>
+                            <p className="font-[600] ml-5 text-[rgb(110,110,110)]">05/2022 – 06/2022</p>
+                        </div>
+                        
+                        <div className="lg:w-[85%] w-full mt-5">
+                            <p className="text-xl">
+                                Local fishers is a freelance project that serve as registration or renewal for boat owners in the province of Orani. My client in this project
+                                is my friend and this is his thesis output. I was the backend developer of this project and we used PHP for development and no frameworks
+                                are used in this project.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="w-full mt-10 lg:px-0 px-20">
+                        <div className="flex flex-row items-center">
+                            <div className="py-2 px-2 shadow-[rgba(0,0,0,0.1)_0px_6px_24px_0px,_rgba(0,0,0,0.4)_0px_0px_0px_1px] rounded-full w-36">
+                                <p className="tex-xl font-[600] text-center">Cinefind</p>
+                            </div>
+                            <p className="font-[600] ml-5 text-[rgb(110,110,110)]">01/2022 – 02/2022</p>
+                        </div>
+                        
+                        <div className="lg:w-[85%] w-full mt-5">
+                            <p className="text-xl">
+                                Cinefind is a web project during my 3rd year college. It is a online movie reservation that is developed using PHP.
+                                This was the very first PHP project I created without any frameworks used.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="w-full mt-10 lg:px-0 px-20">
+                        <div className="flex flex-row items-center">
+                            <div className="py-2 px-2 shadow-[rgba(0,0,0,0.1)_0px_6px_24px_0px,_rgba(0,0,0,0.4)_0px_0px_0px_1px] rounded-full w-32">
+                                <p className="tex-xl font-[600] text-center">Greenboard</p>
+                            </div>
+                            <p className="font-[600] ml-5 text-[rgb(110,110,110)]">04/2022 - 05/2022</p>
+                        </div>
+                        
+                        <div className="lg:w-[85%] w-full mt-5">
+                            <p className="text-xl">
+                                Cinefind is a web project during my 3rd year college. It is a online movie reservation that is developed using PHP.
+                                This was the very first PHP project I created without any frameworks used.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+} 
